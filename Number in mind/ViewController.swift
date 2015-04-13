906ab04c616e2f3b1da17ac2e2e2e5a398a1da47
Nick Ludlow <nickludlow82@gmail.com>
2015-04-13 18:29:02 +0100

    Initial Commit

---
+++ Number in mind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Number in mind
+//
+//  Created by Nick Ludlow on 13/04/2015.
+//  Copyright (c) 2015 Nick Ludlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
